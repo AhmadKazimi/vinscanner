7d7aacd4bff43e973efba7036814fe5609f0a400
Kazimi <ahmad.kazimi@syarah.com>
2025-12-08 10:04:08 +0300

    mark vin with special char in the middle as invalid

--- syaravin-library/src/main/java/com/syarah/vinscanner/data/model/VinValidationResult.kt
+++ syaravin-library/src/main/java/com/syarah/vinscanner/data/model/VinValidationResult.kt
@@ -6,10 +6,12 @@ package com.syarah.vinscanner.data.model
  * @property errorMessage Error message if validation fails
  * @property checksumValid Whether the checksum digit is valid
  * @property formatValid Whether the format is valid
+ * @property wasTrimmed Whether invalid characters were removed from start/end
  */
 internal data class VinValidationResult(
     val isValid: Boolean,
     val errorMessage: String? = null,
     val checksumValid: Boolean = false,
-    val formatValid: Boolean = false
+    val formatValid: Boolean = false,
+    val wasTrimmed: Boolean = false
 )
